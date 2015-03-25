8b37c4497c0919811402149ad11191a0644414d8
David Levayer <david.levayer@gmail.com>
2015-03-25 18:50:31 -0400

    Tab view for the activity + some xml resources

--- app/src/main/java/android/uqacproject/com/suchnote/MainActivity.java
+++ app/src/main/java/android/uqacproject/com/suchnote/MainActivity.java
@@ -13,6 +13,8 @@ import android.location.LocationManager;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.uqacproject.com.suchnote.audiofragment.AudioDialogFragment;
 import android.uqacproject.com.suchnote.photofragment.PhotoDialogFragment;
@@ -20,6 +22,9 @@ import android.uqacproject.com.suchnote.textfragment.TextDialogFragment;
 import android.uqacproject.com.suchnote.videofragment.VideoDialogFragment;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
 
 import java.util.HashMap;
 
@@ -31,7 +36,10 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
     public final static int PHOTO_FRAGMENT_ID = 2;
     public final static int TEXT_FRAGMENT_ID = 3;
 
-    private HashMap<Integer,Object> sensorValues;
+    private ViewPager mViewPager;
+    private SlidingTabLayout mSlidingTabLayout;
+
+    private HashMap<Integer, Object> sensorValues;
     private SensorManager mSensorManager;
     private Sensor mLightSensor;
     private WifiManager mWifiManager;
@@ -40,27 +48,16 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mViewPager.setAdapter(new SamplePagerAdapter());
+        mSlidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
+        mSlidingTabLayout.setViewPager(mViewPager);
 
-        mWifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-
-        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        // Define a listener that responds to location updates
-        mLocationListener = new LocationListener() {
-            public void onLocationChanged(Location location) {
-                location.getLatitude();
-                // TODO save la vitesse
-            }
-            public void onStatusChanged(String provider, int status, Bundle extras) { }
-            public void onProviderEnabled(String provider) { }
-            public void onProviderDisabled(String provider) { }
-        };
-
-
+        initSensors();
     }
 
     @Override
@@ -85,12 +82,12 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
         return super.onOptionsItemSelected(item);
     }
 
-    public void launchDialogFragment(int dialogFragmentId){
+    public void launchDialogFragment(int dialogFragmentId) {
 
         FragmentManager fm = getFragmentManager();
         DialogFragment f = null;
 
-        switch(dialogFragmentId) {
+        switch (dialogFragmentId) {
             case AUDIO_FRAGMENT_ID:
                 f = new AudioDialogFragment();
                 break;
@@ -111,14 +108,14 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
     protected void onResume() {
         super.onResume();
 
-        if(sensorValues == null)
+        if (sensorValues == null)
             sensorValues = new HashMap<>();
 
-        mSensorManager.registerListener(this,mLightSensor,SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mLightSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
         WifiInfo mWifiInfo = mWifiManager.getConnectionInfo();
         String ssid = "none";
-        if(mWifiInfo != null)
+        if (mWifiInfo != null)
             ssid = mWifiInfo.getSSID();
         // TODO save l'id wifi
 
@@ -133,16 +130,101 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
         mLocationManager.removeUpdates(mLocationListener);
     }
 
+    private void initSensors() {
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        mWifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define a listener that responds to location updates
+        mLocationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                location.getLatitude();
+                // TODO save la vitesse
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+        };
+    }
+
     @Override
     public void onSensorChanged(SensorEvent event) {
-        switch(event.sensor.getType()){
+        switch (event.sensor.getType()) {
             case Sensor.TYPE_LIGHT:
-                sensorValues.put(Sensor.TYPE_LIGHT,event.values[0]);
+                sensorValues.put(Sensor.TYPE_LIGHT, event.values[0]);
                 // TODO save la luminosité
                 break;
         }
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) { }
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+
+    class SamplePagerAdapter extends PagerAdapter {
+
+        private ListView mListView;
+        //private NewsArrayAdapter mListViewAdapter;
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object o) {
+            return o == view;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            String[] tabs = getResources().getStringArray(R.array.activity_tabs);
+            switch (position) {
+                case TEXT_FRAGMENT_ID:
+                    return tabs[0];
+                case AUDIO_FRAGMENT_ID:
+                    return tabs[1];
+                case PHOTO_FRAGMENT_ID:
+                    return tabs[2];
+                case VIDEO_FRAGMENT_ID:
+                    return tabs[3];
+            }
+            return "noName";
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+
+            // Inflate a new layout from our resources
+            View view = getLayoutInflater().inflate(R.layout.activity_main_list,
+                    container, false);
+
+            mListView = (ListView) view.findViewById(R.id.note_listview);
+            //mListViewAdapter = new NewsArrayAdapter(mContext, new ArrayList<News>());
+            //mListView.setAdapter(mListViewAdapter);
+            //mListView.setOnItemClickListener(HomeFragment.this);
+
+            // TODO Add element to the list
+
+            // Add the newly created View to the ViewPager
+            container.addView(view);
+
+            // Return the View
+            return view;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+    }
 }
