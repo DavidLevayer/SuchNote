8af4c54c5cbc30ac6f20068829debbca6cb865a6
Corentin Ricou <corentin.ricou@gmail.com>
2015-03-26 18:42:23 -0400

    such photo

--- app/src/main/java/android/uqacproject/com/suchnote/photofragment/PhotoDialogFragment.java
+++ app/src/main/java/android/uqacproject/com/suchnote/photofragment/PhotoDialogFragment.java
@@ -1,22 +1,103 @@
 package android.uqacproject.com.suchnote.photofragment;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.uqacproject.com.suchnote.BasicDialogFragment;
 import android.uqacproject.com.suchnote.R;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by David Levayer on 23/03/15.
  */
-public class PhotoDialogFragment extends BasicDialogFragment {
+public class PhotoDialogFragment extends BasicDialogFragment  {
 
+    /* ---------------- */
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
+    /* ---------------- */
     private View mView;
 
+   /* private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null){
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+            } catch (IOException e) {
+            }
+        }
+    };*/
+
+
+    /* ---------------- */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mView = inflater.inflate(R.layout.dialogfragment_photo, container, false);
+
+        boolean t = checkCameraHardware(getActivity());
+        mCamera = getCameraInstance();
+        mPreview = new CameraPreview(getActivity(), mCamera);
+        FrameLayout preview = (FrameLayout) mView.findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+
+        Button captureButton = (Button) mView.findViewById(R.id.button_capture);
+        /*captureButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // get an image from the camera
+                        mCamera.takePicture(null, null, mPicture);
+                    }
+                }
+        );*/
+
+
         return mView;
     }
+
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
 }
